b8132cc5726b593683883e08320a83eae39170bf
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-02-12 15:53:27 +0100

    main + comments

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+//Las instrucciones que comienzan con # se llaman directivas del preprocesador.
+//La librería iostream, te permite recibir datos de entrada o mostrar datos de salida.
+
+using namespace std;
+//using namespace std, te permite utilizar, en el código, 
+//nombres estandar de c++, para poder importarlas sin necesidad
+//de escribir desde donde la estamos importando
+
+
+//estructura básica de cualquier programa en c++ o c
+//es la función main. Todo programa comienza desde acá
+
+int main () {
+//La función main, es la funcion principal de nuestro código. Es lo primero que ejecuta el programa    
+    cout << "Hola mundo";
+//Para mostrar en pantalla, utilzar cout << "Un texto".
+//cout te permite mostrar en pantalla.
+}
+
+//cada vez que le damos una orden a nuestra computadora por medio de 
+//una linea de código, debemos terminar con un ";"
