f08bd7e721f4f2e22d8f7f355c9eb6ac6d72ecca
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-02-13 23:57:16 +0100

    add contants and lists

---
+++ constantes-lists/constantes-lists.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int  age = 18;
+    age = 28;
+    char letra = 'A';
+    cout << "Hola mundo " << age << endl;
+    cout << letra;
+
+}
+
