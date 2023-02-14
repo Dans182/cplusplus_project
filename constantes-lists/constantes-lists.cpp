5e1e0d5b4b5a6dfd7b02a9be4924c5d7b42bfe7b
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-02-15 00:24:42 +0100

    Constantes y listas

--- constantes-lists/constantes-lists.cpp
+++ constantes-lists/constantes-lists.cpp
@@ -2,12 +2,18 @@
 
 using namespace std;
 
-int main () {
-    int  age = 18;
+int main()
+{
+    int age = 18;
     age = 28;
     char letra = 'A';
     cout << "Hola mundo " << age << endl;
-    cout << letra;
+    // endl es para hace un salto de linea. endl es de la librería iostream
+    cout << letra << endl;
 
+    const char letra2 = 'B';
+    // acá declaramos una constante. valor de solo lectura, no modificable.
+
+    int lista_edades[] = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+    cout << letra2 << endl;
 }
-
