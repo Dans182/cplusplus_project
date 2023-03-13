d2b5b1349c91a756318b19ac6d63e39461f07581
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-03-13 13:14:09 +0100

    operators 2

--- Operators/operators.cpp
+++ Operators/operators.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
 using namespace std;
 
-
-int main(){
+int main()
+{
     int a = 2;
     int b = 3;
     int c = a * b;
     cout << a + b << endl;
-    cout << a - b << endl; 
-    cout << a / b<< endl; 
+    cout << a - b << endl;
+    cout << a / b << endl;
     cout << c << endl;
 
     bool d = a < b;
-    cout << d;
-    //En lenguajes de bajo nivel, cuando trabajas con bools, el verdadero te lo responde
-    //en binario, indicandote "true = 1"  y false = 0
+    cout << d << endl;
+    // En lenguajes de bajo nivel, cuando trabajas con bools, el verdadero te lo responde
+    // en binario, indicandote "true = 1"  y false = 0
     bool e = a > b;
-    cout << e;
+    cout << e << endl;
+
+    a += 1;
+    cout << a << endl;
+
+    cout << sizeof(a);
+    // este operador me indica el tamaño en bites de un valor, de un dato.
+
+    int edades[] = {24, 40, 7};
+    cout << sizeof(a);
+
 }
