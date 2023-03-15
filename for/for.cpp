052667fb03a77eddb8238462d9f8793b17e4258b
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-03-16 00:45:49 +0100

    conditional for done

--- for/for.cpp
+++ for/for.cpp
@@ -7,12 +7,15 @@ int main()
     int list[] = {100, 200, 300, 400, 500};
     int limit = sizeof(list) / sizeof(list[0]);
     // La linea inmediatamente superior es una forma de hacer un "list.length" de JS
-    cout << "Limite ";
+    // con sizeof obtenemos el tamaño en bites de una variable
+    // cout << "Limite ";
     // cin >> limit;
     for (int i = 0; i < limit; i += 1)
     {
         cout << i << " " << list[i] << endl;
+        if (list[i] == 200)
+        {
+            break;
+        }
     }
-
-    // con sizeof obtenemos el tamaño en bites de una variable
 }
