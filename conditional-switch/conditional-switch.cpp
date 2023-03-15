83d956ddf44015ad945b55070516bb8a6c69006e
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-03-15 18:49:44 +0100

    conditional switch added

---
+++ conditional-switch/conditional-switch.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int option = 0;
+    cout << "Indique la opcion: ";
+    cin >> option;
+
+    switch(option){
+        case 1:
+            cout << "Opcion 1";
+            break;
+        case 2:
+            cout << "Opcion 2";
+            break;
+        case 3:
+            cout << "Opcion 3";
+            break;
+        default:
+            cout << "Escogiste un valor que no existe";
+            break;    
+    }
+}
