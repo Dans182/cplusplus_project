60c2a74778eb83a5bb4bf45ca46f2556b4b6e4df
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-03-15 18:17:50 +0100

    logic operators and conditionals added

--- conditionals/conditionals.cpp
+++ conditionals/conditionals.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 
 using namespace std;
-int main(){
-    
+int main()
+{
+
+    bool resultado = true & true;
+    cout << resultado << endl;
+    // "&&" o "and" evalua si los dos lados son verdaderos, devuelve true
+    // si los dos son falsos, devuelve falso. Y si alguno de los dos es verdadero,
+    // devolverá falso.
+
+    bool resultado2 = true || true;
+    cout << resultado2 << endl;
+    // "||" o "or" evalua si los dos lados son verdaderos, devuelve true
+    // si los dos son falsos, devuelve falso. Y si alguno de los dos es verdadero,
+    // devolverá verdadero.
+
+    bool resultado3 = !true;
+    cout << resultado3 << endl;
+    // "!" o "not" invierte el valor lógico que indicaramos a continuación de la exclamación.
+
+    int edad = 0;
+    cout << "Tu edad es: ";
+    cin >> edad;
+
+    if (edad >= 18){
+        cout << "Puedes votar.";
+    } else {
+        cout << "No puedes votar.";
+    }
 }
